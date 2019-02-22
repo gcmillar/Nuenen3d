52c295e25ef2fda74fed0ca30d6b1b3eca672537
gcmillar <gcmillar@ncsu.edu>
2019-02-23 00:17:33 +0100

    correcting unknown error

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Nuenen3d
+## Stress3d
+
+[Active Online Link to Application](https://gcmillar.github.io/stress3d/).
+
+This application uses cyclists' physiological data to see how stress and emotions can be affected by our environment. To begin, use the 'Toggle Cyclists' menu on the left (as shown below) to start exploring their stress levels and associated bike routes. The cyclists highlighted in yellow are those currently being displayed on the map. And be sure to use Street View! With this, it is easier to understand what the cyclists may have been seeing while experiencing high levels of stress.
+
+![Image](img/stress_toggle.gif)
+```
--- js/Nuenen3d.js
+++ js/Nuenen3d.js
@@ -1,129 +1,41 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2NtaWxsYXIiLCJhIjoiY2pvcDhrbGl4MDFvaTNrczR0d2hxcjdnNSJ9.JYgBw6y2pEq_AEAOCaoQpw';
 
-// Load the mapbox map
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/outdoors-v11?optimize=true',
-    center: [5.5509, 51.47686],
-    zoom: 17.5,
-    pitch: 60
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v9',
+center: [-87.61694, 41.86625],
+zoom: 15.99,
+pitch: 40,
+bearing: 20
+});
+ 
+map.on('load', function() {
+map.addLayer({
+'id': 'room-extrusion',
+'type': 'fill-extrusion',
+'source': {
+// GeoJSON Data source used in vector tiles, documented at
+// https://gist.github.com/ryanbaumann/a7d970386ce59d11c16278b90dde094d
+'type': 'geojson',
+'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/indoor-3d-map.geojson'
+},
+'paint': {
+// See the Mapbox Style Specification for details on data expressions.
+// https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions
+ 
+// Get the fill-extrusion-color from the source 'color' property.
+'fill-extrusion-color': ['get', 'color'],
+ 
+// Get fill-extrusion-height from the source 'height' property.
+'fill-extrusion-height': ['get', 'height'],
+ 
+// Get fill-extrusion-base from the source 'base_height' property.
+'fill-extrusion-base': ['get', 'base_height'],
+ 
+// Make extrusions slightly opaque for see through indoor walls.
+'fill-extrusion-opacity': 0.5
+}
+});
 });
 
-// converts from WGS84 Longitude, Latitude into a unit vector anchor at the top left as needed for GL JS custom layers
-var fromLL = function (lon,lat) {
-    // derived from https://gist.github.com/springmeyer/871897
-    var extent = 20037508.34;
-
-    var x = lon * extent / 180;
-    var y = Math.log(Math.tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
-    y = y * extent / 180;
-
-    return [(x + extent) / (2 * extent), 1 - ((y + extent) / (2 * extent))];
-}
-var translate = fromLL(5.55088, 51.476872);
-
-var transform = {
-    translateX: translate[0],
-    translateY: translate[1],
-    translateZ: 0,
-    rotateX: Math.PI / 2,
-    rotateY: 19.925,
-    rotateZ: 0,
-    scale: 4.11843220338983e-8
-}
-
-var THREE = window.THREE;
-
-// Create the Mapbox Custom Layer object
-// See 
-var threeJSModel = {
-    id: 'custom_layer',
-    type: 'custom',
-    onAdd: function(map, gl) {
-        this.camera = new THREE.Camera();
-        this.scene = new THREE.Scene();
-
-        var directionalLight = new THREE.DirectionalLight(0xffffff);
-        directionalLight.position.set(0, -70, 100).normalize();
-        this.scene.add(directionalLight);
-
-        var directionalLight2 = new THREE.DirectionalLight(0xffffff);
-        directionalLight2.position.set(0, 70, 100).normalize();
-        this.scene.add(directionalLight2);
-
-        var loader = new THREE.GLTFLoader();
-        loader.load('gltfmodels/FirstFloor.gltf', (function (gltf) {
-            this.scene.add(gltf.scene);
-        }).bind(this));
-        this.map = map;
-
-        this.renderer = new THREE.WebGLRenderer({
-            canvas: map.getCanvas(),
-            context: gl
-        });
-
-        this.renderer.autoClear = false;
-    },
-    render: function(gl, matrix) {
-        var rotationX = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(1, 0, 0), transform.rotateX);
-        var rotationY = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), transform.rotateY);
-        var rotationZ = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 0, 1), transform.rotateZ);
-
-        var m = new THREE.Matrix4().fromArray(matrix);
-        var l = new THREE.Matrix4().makeTranslation(transform.translateX, transform.translateY, transform.translateZ)
-            .scale(new THREE.Vector3(transform.scale, -transform.scale, transform.scale))
-            .multiply(rotationX)
-            .multiply(rotationY)
-            .multiply(rotationZ);
-
-        this.camera.projectionMatrix.elements = matrix;
-        this.camera.projectionMatrix = m.multiply(l);
-        this.renderer.state.reset();
-        this.renderer.render(this.scene, this.camera);
-        this.map.triggerRepaint();
-    }
-}
-
-map.on('style.load', function() {
-    // map.addLayer({
-    //     'id': '3d-buildings',
-    //     'source': 'composite',
-    //     'source-layer': 'building',
-    //     'filter': ['==', 'extrude', 'true'],
-    //     'type': 'fill-extrusion',
-    //     'minzoom': 15,
-    //     'paint': {
-    //         'fill-extrusion-color': '#ccc',
-    //         'fill-extrusion-height': ["get", "height"]
-    //     }
-    // }, 'waterway-label');
-    map.addSource('Nuenen_all_geojson', {
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/gcmillar/Nuenen3d/master/Nuenen_all_geojson'
-    });
-
-    map.addLayer(threeJSModel, 'waterway-label');
-    map.addLayer({
-        "id": "Nuenen_all",
-        "type": "circle",
-        "source": "Nuenen_all_geojson",
-        "paint": {
-            "circle-radius": {
-                'base': 6,
-                'stops': [[12, 6], [70, 350]],
-            },
-            "circle-opacity": 1,
-            "circle-color": {
-              property: 'conductance_z',
-              type: 'exponential',
-              stops: [
-                [-1,'#2166ac'],
-                [0, '#92c5de'],
-                [1, '#f4a582'],
-                [3, '#b2182b'],
-                ]
-            }
-        },
-        // 'filter': ['==', 'participant', 11]
-    }, 'waterway-label');
-});
+var url = 'https://raw.githubusercontent.com/gcmillar/stress3d/master/CHIPS_all_pts';
